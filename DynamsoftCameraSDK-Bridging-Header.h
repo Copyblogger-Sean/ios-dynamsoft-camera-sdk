f4a41a402606545ac794f5f4b957ba43b9817d79
Copyblogger-Sean <sean@copyblogger.com>
2019-09-07 13:34:32 -0500

    Add files via upload

---
+++ DynamsoftCameraSDK-Bridging-Header.h
@@ -0,0 +1,21 @@
+//
+//  DynamsoftCameraSDK-Bridging-Header.h
+//  
+//
+//  Created by Dynamsoft on 8/22/17.
+//  Copyright © 2017 Dynamsoft. All rights reserved.
+//
+
+#ifndef DynamsoftCameraSDK_Bridging_Header_h
+#define DynamsoftCameraSDK_Bridging_Header_h
+
+#import <DynamsoftCameraSDK/DcsView.h>
+#import <DynamsoftCameraSDK/DcsDocument.h>
+#import <DynamsoftCameraSDK/DcsImage.h>
+#import <DynamsoftCameraSDK/DcsUIImageEditorView.h>
+#import <DynamsoftCameraSDK/DcsException.h>
+#import <DynamsoftCameraSDK/DcsBuffer.h>
+#import <DynamsoftCameraSDK/DcsUIDocumentEditorView.h>
+#import <DynamsoftCameraSDK/DcsUIImageGalleryView.h>
+#import <DynamsoftCameraSDK/DcsUIVideoView.h>
+#endif /* DynamsoftCameraSDK_Bridging_Header_h */
